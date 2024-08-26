0e1b1f8166ba5e944469e1556fe5edd7692f6473
rahbartalab <rahbartalab2002@gmail.com>
2024-08-26 04:06:19 +0330

    [feat] ex-6: a little refactor for routes(without change logic and url)

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MovieController;
+use App\Http\Controllers\StatsController;
 use Illuminate\Support\Facades\Route;
 
 Route
@@ -9,28 +10,33 @@ Route
         return view('welcome');
     });
 
+// this is related to ex-5
 Route
     ::get('login-open-provider', function () {
         \Illuminate\Support\Facades\Auth::attempt();
         dd(\Illuminate\Support\Facades\Auth::user());
     });
 
+// ex-6 routes
 Route
     ::get('/home', [HomeController::class, 'index'])
     ->name('home');
 
 Route
-    ::get('movies', [MovieController::class, 'list_movies'])
-    ->name('list_movies');
+    ::controller(MovieController::class)->group(function () {
+        Route
+            ::get('movies', 'list_movies')
+            ->name('list_movies');
+
+        Route
+            ::get('movie/{id}', 'list_seats')
+            ->name('list_seats');
+
+        Route
+            ::get('movie/{movie}/reserve/{seat}', 'reserve_seat')
+            ->name('reserve_seat');
+    });
 
 Route
-    ::get('movie/{id}', [MovieController::class, 'list_seats'])
-    ->name('list_seats');
-
-Route
-    ::get('movie/{movie}/reserve/{seat}', [MovieController::class, 'reserve_seat'])
-    ->name('reserve_seat');
-
-Route
-    ::get('stats', 'StatsController@stats')
+    ::get('stats', [StatsController::class, 'index'])
     ->name('stats');
